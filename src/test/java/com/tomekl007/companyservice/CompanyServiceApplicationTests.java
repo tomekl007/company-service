9ed996557b53f114b5e8929c28498de177e68e3d
Tomasz Lelek <tomasz.lelek@allegro.pl>
2018-06-20 09:56:09 +0200

    init commit

---
+++ src/test/java/com/tomekl007/companyservice/CompanyServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tomekl007.companyservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CompanyServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
